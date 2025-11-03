3c11c3b50425fedbae6d409d6fe3cc134e5b5b93
Bruno Leite <bruleite.ric@gmail.com>
2025-11-03 19:29:42 -0300

    atividade

---
+++ modelo/assets/js/atividade01.js
@@ -0,0 +1,23 @@
+function calculo(){
+let titulo = document.querySelector('#titulo-01').value;
+let duracao = Number(document.querySelector('#duracao-01').value);
+
+let horas = Math.floor(duracao/60);
+let minutos = duracao % 60;
+
+
+let msgTitulo = document.querySelector('#msgTitulo-01');
+let msgDuracao = document.querySelector('#msgDuracao-01');
+
+msgTitulo.innerText = `Titulo: ${titulo}`;
+msgDuracao.innerText = `Duracao: ${horas} horas(s) e ${minutos}min.`;
+
+}
+
+
+
+let botao = document.querySelector('#botao-01');
+botao.addEventListener('click', function(){
+calculo();
+
+})
